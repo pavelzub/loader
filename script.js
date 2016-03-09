3a609cb9f78faaabd1b058fc3f6a76a325ef51e1
pavelzub <pavelzub1996@gmail.com>
2016-03-09 14:09:25 +1000

    Initial version of loader

---
+++ script.js
@@ -0,0 +1,144 @@
+var link_arr = ['https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1',
+'https://pp.vk.me/c631820/v631820995/1ce61/7COBay5JWx8.jpg', 
+'https://pp.vk.me/c631820/v631820995/1cc89/_1PWAk5UyZg.jpg', 
+'https://pp.vk.me/c628616/v628616995/42515/nkQl5BXzd-M.jpg', 
+'https://pp.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg', 
+'https://pp1.vk.me/c628616/v628616995/4251f/lZ9S3AopZbc.jpg1'],
+	cell_arr = [];
+
+function addTable() {
+      var table = document.getElementById('table');
+		  width = 100,
+          height = 100,
+          rows = 10,
+          columns = 10,
+          tr = "",
+          td = "";
+      table.setAttribute("border", "2px");
+      table.setAttribute("width", width);
+      table.setAttribute("height", height);
+      for (var i = 0; i < rows; i++) {
+        tr = document.createElement("tr");
+        for (var j = 0; j < columns; j++) {
+          td = document.createElement("td");
+		  (function (){
+			var img = document.createElement("img");
+			img.setAttribute("src", "images/2.jpg");
+			img.setAttribute("width", 50);
+			img.setAttribute("height", 50);
+			cell_arr.push(img);
+			td.appendChild(img);
+			tr.appendChild(td);
+			$.get(link_arr[i * 10 + j]).then(
+				function(){ 
+					console.log("$.get succeeded"); 
+					img.setAttribute("src", "images/3.jpg");},
+				function(){ 
+					console.log("$.get succeeded"); 
+					img.setAttribute("src", "images/1.jpg");}
+			);
+		  })();
+        }
+        table.appendChild(tr);
+      }
+}
+
+addTable();
+
+function start_load(){
+
+}
